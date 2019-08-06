bfdb8443352d07c58a60ab573ea5f6b74c2ea94b
Patrick van Staveren <trick@vanstaveren.us>
2019-08-06 15:38:57 -0500

    add handling when api and app key are not specified (the library really should do this.)

--- test.py
+++ test.py
@@ -3,5 +3,10 @@
 from ambient_api.ambientapi import AmbientAPI
 
 api = AmbientAPI()
+if not api.api_key:
+    raise Exception("You must specify an API Key")
+if not api.application_key:
+    raise Exception("You must specify an Application Key")
+
 device = api.get_devices()[0]
 print(device.last_data);
