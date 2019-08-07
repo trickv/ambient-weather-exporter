3b870fe2590bbba924a9bf7c3e8ac99cbd2e033e
Patrick van Staveren <trick@vanstaveren.us>
2019-08-06 22:16:05 -0500

    now run as a real web server and update data every 60 seconds from Ambient API

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import time
+
 from ambient_api.ambientapi import AmbientAPI
 from prometheus_client import Info, Gauge
 from prometheus_client import start_http_server
@@ -10,18 +12,25 @@ if not api.api_key:
 if not api.application_key:
     raise Exception("You must specify an Application Key")
 
-device = api.get_devices()[0]
-last_data = device.last_data
-print(last_data)
+#devices = api.get_devices()
+#if len(devices) == 0:
+#    raise Exception("No weather devices found on your account. Exiting.")
 
 i = Info("ambient_weather_exporter", "Prometheus exporter for Ambient Weather personal weather station")
 i.info({'version': '0'})
 
-g = Gauge("indoor_temperature", "Indoor Temperature in F")
-g.set(last_data['tempinf'])
-g = Gauge("outdoor_temperature", "Outdoor Temperature in F")
-g.set(last_data['tempf'])
+indoor_temperature = Gauge("indoor_temperature", "Indoor Temperature in F")
+outdoor_temperature = Gauge("outdoor_temperature", "Outdoor Temperature in F")
 
 start_http_server(8000)
-import time
-time.sleep(10)
+while True:
+    device = api.get_devices()[0]
+    last_data = device.last_data
+    print(device.info)
+    print(device.mac_address)
+    print(dir(device))
+    print(last_data)
+    indoor_temperature.set(last_data['tempinf'])
+    outdoor_temperature.set(last_data['tempf'])
+    print("sleeping 60");
+    time.sleep(60)
